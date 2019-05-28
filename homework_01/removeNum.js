8c4e15aa501a673278e8cb7d23a8c2cf4a7bde6b
BrhaneWeldeslase <brhaneweldeslase@gmail.com>
2019-05-28 00:17:54 -0500

    solution for question one committed

---
+++ homework_01/removeNum.js
@@ -0,0 +1,23 @@
+Array.prototype.removeNum = function(num){
+
+    let abc= this;
+    return new Promise(function(resolve,reject){
+        resolve(abc.filter(x=>x!=num))
+        
+    });
+
+}
+
+async function processRemoveNum (){
+   try {
+    console.log(await [1,2,3,4,1].removeNum(1));
+       
+   } catch (error) {
+       console.log(error);
+   }
+    
+}
+
+console.log("first");
+processRemoveNum();
+console.log("finally");
