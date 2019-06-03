f24d4478e7e0c8b08174db877cc2b89ad1be3a83
BrhaneWeldeslase <brhaneweldeslase@gmail.com>
2019-06-02 23:28:31 -0500

    Add files via upload
    lab_06 submitted.

---
+++ homework_06/data/grades.js
@@ -0,0 +1,10 @@
+const grades = [
+	{ id: 1, name: 'Brhane', course: 'CS572', grade: 97 },
+	{ id: 2, name: 'Mussie', course: 'CS575', grade: 93 },
+	{ id: 3, name: 'Solomon', course: 'CS572', grade: 71 },
+	{ id: 4, name: 'Sirak', course: 'CS545', grade: 75 },
+	{ id: 5, name: 'Mehari', course: 'CS400', grade: 90 },
+	{ id: 6, name: 'Amanuel', course: 'CS500', grade: 80 },
+];
+
+module.exports = grades;
