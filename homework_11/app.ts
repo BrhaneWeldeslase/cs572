5c305a0372dd096d8ddb074bc2df94367e2a10e5
BrhaneWeldeslase <brhaneweldeslase@gmail.com>
2019-06-11 09:37:01 -0500

    Update app.ts

--- homework_11/app.ts
+++ homework_11/app.ts
@@ -1,7 +1,12 @@
-import {addAvailability} from (./availability)
-
 @addAvailability(true)
-class Course{
+class Course {}
 
+function addAvailability(val) {
+    return function(targetClass) {
+        return class {
+            available = val;
+        }
+    }
 }
+
 console.log(new Course());
